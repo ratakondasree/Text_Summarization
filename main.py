eaa2e32c43b63f79050255e19bec8fdc80ec8616
ratakondasree <sreehari.ratakonda@gmail.com>
2024-09-24 22:50:55 +0530

    data transformation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.logging import logger 
 from pathlib import Path
 
@@ -20,6 +21,17 @@ try:
     logger.info(f">>>>>>>>  Stage {STAGE_NAME} Started>>>>>>>>>>")
     data_validation=DataValidationTrainingPipeline()
     data_validation.main()
+    logger.info(f">>>>>>>>  Stage {STAGE_NAME} Completed >>>>>>>>>>")
+except Exception as e:
+    logger.exception(e)
+    raise e 
+
+STAGE_NAME="Data Transformation Stage"
+
+try:
+    logger.info(f">>>>>>>>  Stage {STAGE_NAME} Started>>>>>>>>>>")
+    data_transformation=DataTransformationTrainingPipeline()
+    data_transformation.main()
     logger.info(f">>>>>>>>  Stage {STAGE_NAME} Completed >>>>>>>>>>")
 except Exception as e:
     logger.exception(e)
